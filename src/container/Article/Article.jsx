8d0b400d5bc7c38f086ad52adbabb0019fd96b9f
JemmyKun <403767069@qq.com>
2019-04-15 18:41:50 +0800

    添加播放控件

--- src/container/Article/Article.jsx
+++ src/container/Article/Article.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Article = () => {
+const Article = (props) => {
+    let { state } = props;
+    console.log(props);
     return (
-        <div className="content-container">Article</div>
+        <div className="content-container" state={state}>Article</div>
     )
 }
 
-export default Article;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        state: state
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        handleDispatch: (action) => {
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Article);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from "react-dom";
 import App from "./container/App";
 import * as serviceWorker from "./serviceWorker";
 import { HashRouter } from "react-router-dom";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+const reducer = (state = [], action) => {
+  return state;
+};
+const middleWares = [logger, thunk];
+const store = createStore(reducer, applyMiddleware(...middleWares));
+
+console.log("store:", store);
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById("root")
 );
 serviceWorker.unregister();
